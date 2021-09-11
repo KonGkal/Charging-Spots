11768fa6875581777f0fbd1b9a69544d840c39f2
KonGkal <konsgkal@gmail.com>
2021-09-11 22:43:10 +0100

    add: mock charging functionality on ChargingPoint

--- Screens/ChargingPoint.tsx
+++ Screens/ChargingPoint.tsx
@@ -3,17 +3,25 @@ import { View, Text, StyleSheet, Pressable } from "react-native";
 import { useParams } from "react-router-native";
 import NavFooter from "../Components/NavFooter";
 import globalStyles from "../globalStyles/globalStyles";
+import { startCharging } from "../services/apiServices";
 
 const ChargingPoint: FC = () => {
   const [charging, setCharging] = useState<boolean>(false);
   const { id, title } = useParams<{ id: string; title: string }>();
+
+  const handleCharging = (): void => {
+    startCharging(1, 1, +id);
+    setCharging(!charging);
+  };
 
   return (
     <View style={globalStyles.container}>
       <View style={styles.contentContainer}>
         <Text style={globalStyles.text}>{title}</Text>
         <Text style={globalStyles.text}>Charging Point Id: {id}</Text>
-        <Pressable>Start Charging</Pressable>
+        <Pressable onPress={handleCharging}>
+          <Text>Start Charging</Text>
+        </Pressable>
       </View>
       <NavFooter />
     </View>
